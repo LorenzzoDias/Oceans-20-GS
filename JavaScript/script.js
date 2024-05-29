69a8d62551f6297fd4db4215abf4857c4ec51736
wendelldsant <wendell_dsantos@hotmail.com>
2024-05-29 00:50:29 -0300

    add-new-function

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -5,19 +5,27 @@ const error_msg_password = document.getElementById('error-message-password');
 const error_msg_password_confirm = document.getElementById('error-message-password-confirm');
 const error_msg_regiao = document.getElementById('error-message-regiao');
 const error_msg_email = document.getElementById('error-message-email');
+const error_msg_celular = document.getElementById('error-message-phone');
 const name_id = document.getElementById('name');
 const username_id = document.getElementById('username');
 const senha_id = document.getElementById('password');
 const senha_confirm_id = document.getElementById('password-confirm');
 const email_id = document.getElementById('email');
+const celular_id = document.getElementById('phone');
 const regiao = document.getElementById('regiao');
 const char_number_id = 5;
 
-function verificaEspacos(inputValue){
+function verificaElementos(function_contem_or_not, InputId, error_msg_id, error_msg_text){
     let has_Space = false;
-    if(inputValue.includes(' ')){
-        console.log('alert');
+    if(function_contem_or_not){
+        error_msg_id.innerHTML = error_msg_text;
+        error_msg_id.setAttribute('style', 'color: red; font-size: 12px');
+        InputId.setAttribute('style', 'border-color: red; border-width: 2px');
         has_Space = true;
+    }
+    else{
+        error_msg_id.innerHTML = '';
+        InputId.setAttribute('style', 'border-color: green; border-width: 2px');        
     }
     return has_Space;
 }
@@ -34,7 +42,10 @@ function containsNumber(inputValue) {
 
 function verificaUsername(){
     let username = username_id.value;
-    if(username.length<10 || verificaEspacos(username)){
+    if(verificaElementos(username.includes(' '), username_id, error_msg_username, 'O ID de usuário não pode conter espaços')){
+        return false;
+    }
+    else if(username.length<char_number_id){
         error_msg_username.innerHTML = `O ID de usuário deve ter no mínimo ${char_number_id} caracteres e não pode conter espaços` ;
         error_msg_username.setAttribute('style', 'color: red; font-size: 12px');
         username_id.setAttribute('style', 'border-color: red; border-width: 2px');
@@ -158,15 +169,47 @@ function verificaRegiao(){
 
 function verificaEmail(){
     let email = email_id.value;
-    verificaEspacos(email){
-        
+    if(verificaElementos(email.includes(''), email_id, error_msg_email, 'Preencha esse campo.')){
+        return false;
+    }
+    else if(verificaElementos(email.includes(' '), email_id, error_msg_email, 'O email não pode conter espaços'))  {
+        return false;
+    }
+    else if(!verificaElementos(!email.includes('@'), email_id, error_msg_email, 'O email não é válido')){
+        return false;
+    }
+    else if(!verificaElementos(!email.includes('.com'), email_id, error_msg_email, 'O email não é válido')){
+        return false;
+    }
+    else{
+        return true;
     }
 
 }
+
+function verificaCelular(){
+    let celular = celular_id.value;
+    let numbers = [0,1,2,3,4,5,6,7,8,9]
+    if(verificaElementos(celular.includes(''), celular_id, error_msg_celular, 'Preencha esse campo.')){
+        return false;
+    }
+    else if(verificaElementos(celular.includes(' '), celular_id, error_msg_celular, 'O campo não pode conter espaços.')){
+        return false;
+    }
+    else if(!containsNumber(celular)){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
 btn_cadastrar.addEventListener('click', function(event){
     event.preventDefault();
     console.log(verificaUsername());
     console.log(verificaName());
     console.log(verificaSenha());
     console.log(verificaRegiao());
+    console.log(verificaEmail());
+    console.log(verificaCelular());
 })
